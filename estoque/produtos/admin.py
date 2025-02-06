c2b0e5152681b34fa40dfc722bd7783e6b0de3b1
RamonKel <ramon.kelvin04@gmail.com>
2025-02-06 16:01:39 -0300

    criacao de tabelas e usuarios

---
+++ estoque/produtos/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import CustomUser
+
+# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'tipo_usuario', 'data_nascimento']
+    search_fields = ['username', 'email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ estoque/produtos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class Produto (models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    quantidade = models.IntegerField()
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    def __str__ (self):
+        return self.nome
+    
+class CustomUser(AbstractUser):
+    tipo_usuario = models.CharField(max_length=50)
+    data_nascimento = models.DateField(null=True, blank=True)
